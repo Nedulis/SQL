fb6b2d36615f10f01cc8701245222a06ac1c0f8b
Nedas <89401655+Nedulis@users.noreply.github.com>
2021-09-24 00:06:08 +0300

    Add files via upload
    .cs files(code) from automatic testing case

---
+++ AUTO/Drivers/CustomDriver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace autotests.Drivers
+{
+    public class CustomDriver
+    {
+        public static IWebDriver GetChrome()
+        {
+            return GetDriver(Browsers.Chrome);
+        }
+        private static IWebDriver GetDriver(Browsers webDriver)
+        {
+            IWebDriver driver = null;
+            switch (webDriver)
+            {
+                case Browsers.Chrome:
+                    driver = new ChromeDriver();
+                    break;
+            }
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            return driver;
+        }
+    }
+}
---
+++ AUTO/Test/BATest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+namespace autotests.Test
+{
+    public class BATest : BaseTest
+    {
+        [Test]
+        public static void TestBA()
+        {
+            testPage.NavigateToPage();
+            testPage.SearchForItemByText();
+            testPage.CheckItemText();
+            testPage.SelectMugOptions();
+            testPage.CartOptions();
+            testPage.FillingPersonalInfo();
+        }
+    }
+}
