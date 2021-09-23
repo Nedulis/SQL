41f9fda0c940638ed6b460d2e1e2600b511d67d7
Nedas <89401655+Nedulis@users.noreply.github.com>
2021-09-23 23:48:32 +0300

    Add files via upload

---
+++ AUTO/Test/BATest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+namespace autotests.Test
+{
+    public class BATest : BaseTest
+    {
+        
+        [Test]
+        public static void TestDecatlonSearchOrderByValue()
+        {
+            testPage.NavigateToPage();
+            testPage.SearchForItemByText();
+            testPage.CheckItemText();
+            testPage.SelectMugOptions();
+            testPage.CartOptions();
+            testPage.FillingPersonalInfo();
+        }
+    }
+}
